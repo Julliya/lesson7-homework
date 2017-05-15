bd6bfb4ef246ed324139663dee526931d41c94e2
Julli Babenko <julli.babenko@gmail.com>
2017-05-15 09:56:05 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var n = Number(prompt('Введіть номер числа Фібоначчі'));
+
+if (Number.isNaN(n)) {
+    document.write('Ви помилились! Введіть, будьласка, число.');
+} else {
+    var k = 0;
+    var m = 1;
+    for (var i = 0; i < n; i++) {
+        var t = k;
+        k = k + m;
+        m = t;
+    }
+    document.write("Значення числа Фібоначчі під номером " + n + " = " + k);
+}
---
+++ js/recursion.js
@@ -0,0 +1,15 @@
+var n = Number(prompt('Введіть номер числа Фібоначчі'));
+
+if (Number.isNaN(n)) {
+    document.write('Ви помилились! Введіть, будьласка, число.');
+} else {
+    var result = fib(n);
+    document.write("Значення числа Фібоначчі під номером " + n + " = " + result);
+}
+function fib(n) {
+    if (n < 3) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
